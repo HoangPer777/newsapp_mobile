d076ca2a7ad44eb8c271b37e7ebae7f9c41c6417
22130189-NguyenBaoNguyen <22130189@st.hcmuaf.edu.vn>
2025-12-30 12:33:33 +0700

    thông báo

--- lib/features/notification/services/notification_service.dart
+++ lib/features/notification/services/notification_service.dart
@@ -4,7 +4,7 @@ import '../model/article_notification.dart';
 
 class NotificationService {
   static const String baseUrl =
-      'http://localhost:8080/api/articles/notifications/new';
+      'http://10.0.2.2:8080/api/articles/notifications/new';
   // ⚠ Android emulator dùng 10.0.2.2 thay localhost
 
   static Future<List<ArticleNotification>> fetchNotifications() async {
