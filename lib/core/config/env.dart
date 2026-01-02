f4820ca2233d0e6d6e01d23a713d80950d23b963
HoangPer007 <22130088@st.hcmuaf.edu.vn>
2026-01-02 23:48:28 +0700

    env

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -3,17 +3,21 @@ class Env {
   static const apiBase = String.fromEnvironment(
     'API_BASE',
     defaultValue:
-        // 'http://172.20.10.3:8080', // chạy ipconfig trong terminal rồi lấy ip IPv4 Address. đảm bảo máy tính và điện thoại cùng 1 mạng
-        'http://10.0.129.45:8080',
+        // 'http://172.20.10.3:8080', // iphone // chạy ipconfig trong terminal rồi lấy ip IPv4 Address. đảm bảo máy tính và điện thoại cùng 1 mạng
+        'http://10.0.70.228:8080', // ktx
+    // 'http://10.0.70.228:8080',
+
     // 'http://10.0.2.2:8080',
     // 'http://localhost:8080',
   );
-  static const chatbotApiBase = String.fromEnvironment('CHATBOT_API_BASE',
-      defaultValue:
-          // 'http://172.20.10.3:8000',
-          'http://10.0.129.45:8000'
-      // usage: same host as apiBase but port 8000
-      );
+  static const chatbotApiBase = String.fromEnvironment(
+    'CHATBOT_API_BASE',
+    defaultValue:
+        // 'http://172.20.10.3:8000',
+        'http://10.0.70.228:8080',
+    // 'http://10.0.70.228:8000'
+    // usage: same host as apiBase but port 8000
+  );
 }
 
 // Dev Mode:
